24580c3071a30e03a78054bbdd5e3cc967701395
Spiky Mike <Mike973A@gmail.com>
2021-08-20 16:28:04 -0400

    the whole fucking app lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Spiky", password: "Itosver9k!")
+User.create(username: "Spiky1", password: "Itosver9k!")
+User.create(username: "Spiky2", password: "Itosver9k!")
+User.create(username: "Spiky3", password: "Itosver9k!")
+User.create(username: "Spiky4", password: "Itosver9k!")
+
+Message.create(body: "I like cats", user: User.find(1))
+Message.create(body: "I like dogs", user: User.find(2))
+Message.create(body: "I like birds", user: User.find(3))
+Message.create(body: "I like cheese", user: User.find(4))
+Message.create(body: "I like Zelda", user: User.find(5))
